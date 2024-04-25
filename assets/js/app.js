d4259633bcced1dd898a3dc5e908e618be3e2876
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2024-04-25 17:39:10 +0200

    create js and add screenshots

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+// Selectors
+const button = document.querySelectorAll('.accordion-container-header');
+
+// Iterate buttons of accordion
+button.forEach(element => {
+	element.addEventListener('click', e => {
+		e.preventDefault();
+
+		const idButton = element.getAttribute('aria-controls');
+		const content = document.getElementById(idButton);
+		const id = idButton.split('-')[2];
+		const icon = document.getElementById(id);
+
+		// Show / hide questions' content and change icon
+		if (content.hidden) {
+			content.hidden = false;
+			icon.src = '/assets/images/icon-minus.svg';
+		} else {
+			content.hidden = true;
+			icon.src = '/assets/images/icon-plus.svg';
+		}
+	});
+});
